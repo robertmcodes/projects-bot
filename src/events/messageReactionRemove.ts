b36eb713c92f3b966f060322433cbc0bcb02ccaf
Linus Willner <hello@linuswillner.me>
2020-07-23 19:35:41 +0300

    Self-check reactions

--- src/events/messageReactionRemove.ts
+++ src/events/messageReactionRemove.ts
@@ -1,9 +1,10 @@
 import Discord from 'discord.js'
 
 export default (client: Discord.Client, reaction: Discord.MessageReaction, user: Discord.User): void => {
+  const isNotSelf = user.id !== client?.user?.id
   const isInSubmissionChannel = reaction.message.channel.id === process.env.PROJECT_SUBMISSIONS_CHANNEL
 
-  if (isInSubmissionChannel) {
+  if (isNotSelf && isInSubmissionChannel) {
     // TODO: Check who user is and add downvote or remove
   }
 }
