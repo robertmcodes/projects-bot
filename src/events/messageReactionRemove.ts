3a930e78e6c30b5fd9137413217b8a63e55dbcc3
Linus Willner <hello@linuswillner.me>
2020-10-12 17:30:14 +0300

    Check that project exists when adding or removing votes

--- src/events/messageReactionRemove.ts
+++ src/events/messageReactionRemove.ts
@@ -1,6 +1,6 @@
 import Discord from 'discord.js'
 import safeSendMessage from '../utils/safeSendMessage'
-import { adjustUpvotesForProject, adjustDownvotesForProject } from '../db'
+import { getProject, adjustUpvotesForProject, adjustDownvotesForProject } from '../db'
 
 export default async (client: Discord.Client, reaction: Discord.MessageReaction, user: Discord.User): Promise<Discord.Message | undefined> => {
   // Ensure reaction was not added in DM, even though the ID check would already technically speaking prevent this
@@ -11,7 +11,15 @@ export default async (client: Discord.Client, reaction: Discord.MessageReaction,
     const isInSubmissionChannel = channel.id === process.env.PROJECT_SUBMISSIONS_CHANNEL
     const isVoteEmoji = reaction.emoji?.id === process.env.UPVOTE_REACTION || reaction.emoji.id === process.env.DOWNVOTE_REACTION
 
-    if (isNotSelf && isInSubmissionChannel && isVoteEmoji) {
+    let projectExists
+
+    try {
+      projectExists = !!(await getProject(id))
+    } catch (err) {
+      return await safeSendMessage(channel, '⚠️ Your vote was not possible to remove. (Failed to validate that message is project)')
+    }
+
+    if (isNotSelf && isInSubmissionChannel && projectExists && isVoteEmoji) {
       let member
 
       // Get reacting member
